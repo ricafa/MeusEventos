9ffbe48206ff44841ae0ffc1582656b606499bd5
Ricardo Camargo Favaro <ri_ca_fa@hotmail.com>
2022-04-02 18:01:00 -0300

    add Evento. put method

---
+++ Back/src/MeusEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MeusEventos.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MeusEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+            
+        }
+
+        public IEnumerable<Evento> _evento = new Evento[] 
+            { 
+                new Evento()
+                {
+                    EventoId = 1,
+                    Tema = "Confraternização",
+                    Local = "Home Sweet Home",
+                    QtdPessoas = 250,
+                    Lote       = "001",
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                    ImagemURL   = "Foto.png",
+                },
+                new Evento()
+                {
+                    EventoId = 2 ,
+                    Tema = "Churrasco",
+                    Local = "AABB",
+                    QtdPessoas = 400,
+                    Lote       = "001",
+                    DataEvento = DateTime.Now.AddDays(18).ToString("dd/MM/yyyy"),
+                    ImagemURL   = "Foto2.png",
+                }
+
+            };
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+        [HttpPut("{id}")]
+        public String Put(int id)
+        {
+            return $"Exemplo de Put. Id = {id}";
+        }
+    }
+}
