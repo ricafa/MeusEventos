5cf31dc5a5dcf1a80893cbe4ebef2e3f2bc3607b
Ricardo Camargo Favaro <ri_ca_fa@hotmail.com>
2022-04-04 18:52:35 -0300

    get by id works trought database

--- Back/src/MeusEventos.API/Controllers/EventoController.cs
+++ Back/src/MeusEventos.API/Controllers/EventoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MeusEventos.API.Data;
 using MeusEventos.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -12,46 +13,22 @@ namespace MeusEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
-            
+            _context = context;
         }
-
-        public IEnumerable<Evento> _evento = new Evento[] 
-            { 
-                new Evento()
-                {
-                    EventoId = 1,
-                    Tema = "Confraternização",
-                    Local = "Home Sweet Home",
-                    QtdPessoas = 250,
-                    Lote       = "001",
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                    ImagemURL   = "Foto.png",
-                },
-                new Evento()
-                {
-                    EventoId = 2 ,
-                    Tema = "Churrasco",
-                    Local = "AABB",
-                    QtdPessoas = 400,
-                    Lote       = "001",
-                    DataEvento = DateTime.Now.AddDays(18).ToString("dd/MM/yyyy"),
-                    ImagemURL   = "Foto2.png",
-                }
-
-            };
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
         public IEnumerable<Evento> GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.Where(evento => evento.EventoId == id);
         }
 
         [HttpPut("{id}")]
