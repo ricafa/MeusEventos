f548023e15a5c2994b35f15097aa7637a997e31c
Ricardo Camargo Favaro <ri_ca_fa@hotmail.com>
2022-04-04 21:27:38 -0300

    first component and ng for

--- Front/MeusEventos-App/src/app/eventos/eventos.component.ts
+++ Front/MeusEventos-App/src/app/eventos/eventos.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
+  public eventos: any = [
+    {
+      Tema: ".NET 5",
+      Local: "Belo Horizonte",
+    },
+    {
+      Tema: "Angular",
+      Local: "São Paulo",
+    },
+    {
+      Tema: "News",
+      Local: "Rio de Janeiro",
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
