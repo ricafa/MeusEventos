debadb73118c1059248c7f8ac9600f48a4f58268
Ricardo Camargo Favaro <ri_ca_fa@hotmail.com>
2022-04-05 21:20:36 -0300

    connecting and listing events from api

--- Front/MeusEventos-App/src/app/app.module.ts
+++ Front/MeusEventos-App/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,13 +11,14 @@ import { EventosComponent } from './eventos/eventos.component';
 @NgModule({
   declarations: [
     AppComponent,
-      PalestrantesComponent,
-      EventosComponent
+    PalestrantesComponent,
+    EventosComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
