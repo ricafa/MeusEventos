6d2089e8d9cebae7d8b9de0c190053df9b1ea0e1
Ricardo Camargo Favaro <ri_ca_fa@hotmail.com>
2022-04-06 21:04:14 -0300

    add navbar

--- Front/MeusEventos-App/src/app/app.module.ts
+++ Front/MeusEventos-App/src/app/app.module.ts
@@ -2,23 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { EventosComponent } from './eventos/eventos.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PalestrantesComponent,
-    EventosComponent
+    EventosComponent,
+      NavComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
